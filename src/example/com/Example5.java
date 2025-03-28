d8ba8507d76fa7a3af57a95f0c3826dff18ce3cd
Przemus14s <przemekszynal5@gmail.com>
2025-03-28 17:13:29 +0100

    new function 'example5'

---
+++ src/example/com/Example5.java
@@ -0,0 +1,29 @@
+package example.com;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public class Example5 {
+    public static void main(String[] args) throws AWTException {
+        Robot robot = new Robot();
+        robot.delay(3000);
+
+        String text = "Automatyczne pisanie z użyciem klasy Robot!";
+
+        typeText(robot, text);
+        System.out.println("Tekst został wpisany automatycznie!");
+    }
+
+    public static void typeText(Robot robot, String text) {
+        for (char c : text.toCharArray()) {
+            int keyCode = KeyEvent.getExtendedKeyCodeForChar(c);
+            if (KeyEvent.CHAR_UNDEFINED == keyCode) {
+                continue;
+            }
+            robot.keyPress(keyCode);
+            robot.keyRelease(keyCode);
+            robot.delay(100);
+        }
+    }
+}
+
